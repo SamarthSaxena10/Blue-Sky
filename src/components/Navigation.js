cb9625df2c0d3454ab68b5b4cf64e6be43a03b6a
Samarth <samkusaxena@gmail.com>
2023-02-19 10:12:19 +0530

    Added changed in js file

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,75 +1,45 @@
-import { ethers } from 'ethers';
-import logo from '../assets/logo.svg';
+import { ethers } from "ethers";
+import logo from "../assets/logo.svg";
 
 const Navigation = ({ account, setAccount }) => {
-    const connectHandler = async () => {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const account = ethers.utils.getAddress(accounts[0])
-        setAccount(account);
-    }
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+  };
 
-    return (
-<nav >
-  <div class="max-w-6xl mx-auto px-4">
-    <div class="flex justify-between">
+  return (
+    <nav>
+      <ul className="nav__links">
+        <li>
+          <a href="#">Buy</a>
+        </li>
+        <li>
+          <a href="#">Rent</a>
+        </li>
+        <li>
+          <a href="#">Sell</a>
+        </li>
+      </ul>
 
-      <div class="flex space-x-4">
-        <div>
-          <a href="#" class="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900">
-            <img src={logo} class="h-12 w-12 mr-1"/>
-            <span class="font-bold">Millow</span>
-          </a>
-        </div>
-
-        <div class="hidden md:flex items-center space-x-1">
-          <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900">Buy</a>
-          <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900">Rent</a>
-          <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900">Sell</a>
-
-        </div>
+      <div className="nav__brand">
+        <img src={logo} alt="Logo" />
+        <h1>Brick and Walls</h1>
       </div>
 
-      <div class="hidden md:flex items-center space-x-1">
       {account ? (
-                <button
-                    type="button"
-                    className='py-2 px-3 bg-violet-600 hover:bg-violet-500 text-white hover:text-violet-100 rounded-lg transition duration-300'
-                >
-                    {account.slice(0, 6) + '...' + account.slice(38, 42)}
-                </button>
-            ) : (
-                <button
-                    type="button"
-                    className='py-2 px-3 bg-violet-600 hover:bg-violet-500 text-white hover:text-violet-100 rounded-lg transition duration-300'
-                    onClick={connectHandler}
-                >
-                    Connect
-                </button>
-            )}
-        {/* <a href="" class="py-2 px-3 bg-violet-600 hover:bg-violet-500 text-white hover:text-violet-100 rounded-lg transition duration-300">Signup</a> */}
-      </div>
-
-      <div class="md:hidden flex items-center">
-        <button class="mobile-menu-button">
-          <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
+        <button type="button" className="nav__connect">
+          {account.slice(0, 6) + "..." + account.slice(38, 42)}
         </button>
-      </div>
-
-    </div>
-  </div>
-
-  <div class="mobile-menu hidden md:hidden">
-    <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">Buy</a>
-    <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">Rent</a>
-    <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">Sell</a>
-
-  </div>
-</nav>
-    );
-}
+      ) : (
+        <button type="button" className="nav__connect" onClick={connectHandler}>
+          Connect
+        </button>
+      )}
+    </nav>
+  );
+};
 
 export default Navigation;
-
-
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,14 @@
 const Search = () => {
-    return (
-        <header className="justify-self-center content-center pt-10">
-            {/* <h2 className="header__title">Search it. Explore it. Buy it.</h2>
-            <input
-                type="text"
-                className="header__search"
-                placeholder="Enter an address, neighborhood, city, or ZIP code"
-            /> */}
-            <h1 class="text-center font-sans text-5xl py-16 font-semibold">Search it. Explore it. Buy it.</h1>
-
-            <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-            <div class="flex justify-center content-center ">
-                <input type="search" id="search" class=" block w-1/2 p-4 text-sm text-gray-900 border-none dark:border-none ring-0 dark:ring-0 rounded-lg bg-Slate-50  dark:bg-Slate-700  dark:placeholder-gray-400 dark:drop-shadow-lg dark:text-white" placeholder="Enter an address, neighborhood, city, or ZIP code" required />
-                {/* <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> */}
-            </div>
-        </header>
-    );
-}
+  return (
+    <header>
+      <h2 className="header__title">Search it. Explore it. Buy it.</h2>
+      <input
+        type="text"
+        className="header__search"
+        placeholder="Enter an address, neighborhood, city, or ZIP code"
+      />
+    </header>
+  );
+};
 
 export default Search;
